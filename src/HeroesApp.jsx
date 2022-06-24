d5f2f317506e6de8b2ba0471e10289b540187ecd
Daniel González Briseño <daniel.gb@globant.com>
2022-06-24 16:23:05 -0500

    15 - AuthContext, AuthReducer

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,10 +1,11 @@
+import { AuthProvider } from "./auth";
 import { AppRouter } from "./router";
 
 const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
       <AppRouter />
-    </>
+    </AuthProvider>
   );
 };
 
