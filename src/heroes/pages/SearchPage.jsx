d08f832a4f95af81c5ed2ce03e9b9537f018c121
Daniel González Briseño <daniel.gb@globant.com>
2022-06-24 14:34:18 -0500

    13 - Query Params

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,11 +1,29 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import queryString from "query-string";
+
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const { search } = useLocation();
+
+  const { q = '' } = queryString.parse( search );
+
+  const { searchText, onInputChange } = useForm({ searchText: '' });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.length <= 2 ) return;
+    navigate(`?q=${searchText.trim()}`);
+  };
+
   return (
     <>
       <h1>Search a Hero</h1>
       <hr />
       <div className="row mt-4">
         <div className="col-12 col-lg-5">
-          <form>
+          <form onSubmit={ onSearchSubmit }>
             <div className="row g-2 align-items-center mb-4 mb-lg-0">
               <div className="col-12 col-md-10">
                 <input
@@ -15,12 +33,14 @@ export const SearchPage = () => {
                   type="text"
                   autoComplete="off"
                   placeholder="Search a hero"
+                  value={ searchText }
+                  onChange={ onInputChange }
                 />
               </div>
               <div className="col-12 col-md-2">
                 <div className="d-grid">
                   <button type="submit" className="btn btn-outline-primary">
-                    <i class="bi bi-search"></i>
+                    <i className="bi bi-search"></i>
                   </button>
                 </div>
               </div>
@@ -31,12 +51,12 @@ export const SearchPage = () => {
           <h2 style={{ lineHeight: '28px', marginBottom: '0' }}>Results</h2>
           <hr />
 
-          <div className="alert alert-info fw-bold">
-            <i class="bi bi-emoji-wink"></i> Type a hero name to get results
+          <div className="alert alert-info">
+            <i className="bi bi-emoji-wink"></i> Type a hero name to get results
           </div>
 
-          <div className="alert alert-danger fw-bold">
-            <i class="bi bi-emoji-frown-fill"></i> There's no hero named "Goku"
+          <div className="alert alert-danger">
+            <i className="bi bi-emoji-frown-fill"></i> There's no hero named <b>{ q }</b>
           </div>
         </div>
       </div>
