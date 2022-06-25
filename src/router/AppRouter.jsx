f82c07f2c3e62609c31e0ce9557e71a934870b86
Daniel González Briseño <daniel.gb@globant.com>
2022-06-24 23:31:46 -0500

    18 - Public and Private Routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,23 @@ import { Routes, Route } from "react-router-dom";
 
 import { LoginScreen } from "../auth";
 import { HeroesRoutes } from "../heroes";
+import { PublicRoutes, PrivateRoutes } from "./";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={ <LoginScreen /> } />
-        <Route path="*" element={ <HeroesRoutes /> } />
+        <Route path="login" element={
+          <PublicRoutes>
+            <LoginScreen />
+          </PublicRoutes>
+        } />
+
+        <Route path="*" element={
+          <PrivateRoutes>
+            <HeroesRoutes />
+          </PrivateRoutes>
+        } />
       </Routes>
     </>
   );
